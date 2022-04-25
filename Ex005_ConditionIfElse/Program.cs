649490cbae58e71bcd870c459b90d68f9932496c
Sergey <perfer79@gmail.com>
2022-04-25 22:26:28 +0700

    Add ex005_ConditionIfElse

---
+++ Ex005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿// Комментарий к коду не относящийся. Заметки, памятки
+// ToLower - переводит все символы в нижний регистр (Маша, МАша ...)
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
